be5e0834e737a0bae40f09c34e5be9b81dc8bb01
devwraithe <folahanmi0001@gmail.com>
2025-12-24 21:42:45 +0100

    feat: add deposit instruction

--- programs/atharva_refi/src/errors.rs
+++ programs/atharva_refi/src/errors.rs
@@ -4,4 +4,12 @@ use anchor_lang::prelude::*;
 pub enum ErrorCode {
     #[msg("Only the designated admin can create pools.")]
     Unauthorized,
+    #[msg("Arithmetic overflow")]
+    MathError,
+    #[msg("Pool is currently not active")]
+    PoolNotActive,
+    #[msg("Not enough SOL to deposit")]
+    InsufficientFunds,
+    #[msg("Invalid amount")]
+    InvalidAmount,
 }
--- programs/atharva_refi/src/instructions/mod.rs
+++ programs/atharva_refi/src/instructions/mod.rs
@@ -1,3 +1,5 @@
 pub mod create_pool;
+pub mod deposit;
 
 pub use create_pool::*;
+pub use deposit::*;
